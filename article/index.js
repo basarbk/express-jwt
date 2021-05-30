cf370933358319ef7bf761fbd42f45659fe3e3bc
Basar Buyukkahraman <basarbk@yahoo.com>
2021-05-30 15:25:51 +0300

    Implement JWT logic

--- article/index.js
+++ article/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
@@ -11,7 +12,15 @@ app.get("/api/1.0/articles", (req, res) => {
 })
 
 app.post("/api/1.0/articles", (req, res) => {
-  // 
+  const authorization = req.headers.authorization
+  const token = authorization.substring(7);
+  const result = jwt.verify(token, "this-is-our-secret")
+  const article = {
+    ...req.body,
+    userId: result.id
+  }
+  articles.push(article);
+  res.send({message: "Success"})
 })
 
 app.listen(3001, () => {
--- auth/index.js
+++ auth/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
@@ -16,7 +17,18 @@ for(let i = 1; i <= 5; i++) {
 }
 
 app.post("/api/1.0/auth", (req, res) => {
-  //
+  const credentials = req.body;
+  const user = users.find(user => user.email === credentials.email);
+  if(!user) {
+    return res.status(401).send({message: "Unauthorized"});
+  }
+  if(user.password !== credentials.password) {
+    return res.status(401).send({message: "Unauthorized"});
+  }
+  const token = jwt.sign({ id: user.id }, 'this-is-our-secret')
+  res.send({
+    token: token
+  })
 })
 
 
